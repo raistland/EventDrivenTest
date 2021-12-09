a9bbbc0286c7d8354efde36e1167577e5fc26abf
raistland <raistland@gmail.com>
2021-12-09 23:51:02 +0300

    Init

---
+++ PostService/Data/PostServiceContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostService.Entities;
+
+namespace PostService.Data
+{
+    public class PostServiceContext : DbContext
+    {
+        public PostServiceContext(DbContextOptions<PostServiceContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Post> Post { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasOne(k=>k.User);
+        }
+    }
+}
---
+++ UserService/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string OtherData { get; set; }
+    }
+}
